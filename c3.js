211e0e4fbc27c7e295cb16a2aaf2d3e178006b7b
MuhamadFathir <fathirrizqii866@gmail.com>
2025-04-10 20:14:23 +0700

    done

--- c3.js
+++ c3.js
@@ -1,12 +1,24 @@
 function romawi(n) {
+    const angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const simbol = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
+    let hasil = "";
+
+    for (let i = 0; i < angka.length; i++) {
+        while (n >= angka[i]) {
+            hasil += simbol[i];
+            n -= angka[i];
+        }
+    }
+
+    return hasil;
 }
 
 console.log("Script Testing untuk Konversi Romawi\n");
 console.log("input  | expected  | result");
 console.log("------ | --------  | ------");
-console.log("4      | IV        | ", romawi(4));
-console.log("9      | IX        | ", romawi(9));
-console.log("13     | XIII      | ", romawi(13));
-console.log("1453   | MCDLIII   | ", romawi(1453));
-console.log("1646   | MDCXLVI   | ", romawi(1646));
+console.log("4      | IV        |", romawi(4));
+console.log("9      | IX        |", romawi(9));
+console.log("13     | XIII      |", romawi(13));
+console.log("1453   | MCDLIII   |", romawi(1453));
+console.log("1646   | MDCXLVI   |", romawi(1646));
