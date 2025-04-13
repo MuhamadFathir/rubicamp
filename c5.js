7c54068e43d91afe802adbd7bad44b8e175fd3d2
MuhamadFathir <fathirrizqii866@gmail.com>
2025-04-13 21:12:56 +0700

    done

---
+++ c5.js
@@ -0,0 +1,11 @@
+function stringManipulation(word) {
+    if (word.startsWith("a", "i", "u", "e", "o")) {
+        console.log(word)
+    } else {
+        console.log(word.slice(1).concat(word[0], "nyo"))
+    }
+
+}
+
+stringManipulation("ayam")
+stringManipulation("bebek")
