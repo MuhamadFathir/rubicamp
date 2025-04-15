b1bb70ecc43fe8c9b61bddc5f5ddedddd68a9e6f
MuhamadFathir <fathirrizqii866@gmail.com>
2025-04-15 13:52:18 +0700

    done

--- c5.js
+++ c5.js
@@ -1,11 +1,11 @@
 function stringManipulation(word) {
-    if (word.startsWith("a", "i", "u", "e", "o")) {
+    if ("aiueo".includes(word[0].toLowerCase())) {
         console.log(word)
     } else {
         console.log(word.slice(1).concat(word[0], "nyo"))
     }
 
 }
-
-stringManipulation("ikan")
-stringManipulation("bebek")
+stringManipulation("ikan dan ayam")
+stringManipulation("ayam")
+stringManipulation("bebek dan anjing")
--- c6.js
+++ c6.js
@@ -1,4 +1,20 @@
-function sentencesManipulation(sentence){
-    console.log(sentence.replace(/p/, ""))
+function stringManipulation(word) {
+    if ("aiueo".includes(word[0].toLowerCase())) {
+        return word
+    } else {
+        return word.slice(1).concat(word[0], "nyo")
+    }
+
 }
+
+
+function sentencesManipulation(sentence) {
+    let hasil = []
+    let test = sentence.split(" ")
+    for (let i = 0; i < test.length; i++) {
+        hasil.push(stringManipulation(test[i]))
+    }
+    console.log(hasil.toString().replace(/,/g, " "))
+}
+
 sentencesManipulation("ibu pergi ke pasar bersama aku")
