2c6fb1c7da56c7476a99c5a8117c62a5a1ce00d7
MuhamadFathir <fathirrizqii866@gmail.com>
2025-04-16 17:49:08 +0700

    new

--- c8.js
+++ c8.js
@@ -1,6 +1,14 @@
-function pola(str){
+function pola(str) {
+    let hasil = []
+    let test = str.split(" ")
+    console.log(test.toString().replace("#", 8))
     
+    // for (let i = 0; i <= test.length; i++) {
+    //     for (let j = 0; j <= test.length; j++) {
+            
+    //     }
+    // }
 }
 
-console.log(pola("32#3 * 188 = 80#204"))
-console.log(pola("8#61 * 895 = 78410#5"))
+console.log(pola("42#3 * 188 = 80#204"))
+// console.log(pola("8#61 * 895 = 78410#5"))
