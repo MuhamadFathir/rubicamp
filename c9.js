cbecfae8af7c4e102f293587d8633d43623a02af
MuhamadFathir <fathirrizqii866@gmail.com>
2025-04-19 15:45:08 +0700

    new

--- c9.js
+++ c9.js
@@ -1,15 +1,13 @@
 function spiral(param1) {
     let arr = []
     let num = 0
-    for (let i = 0; i <= param1; i++) {
-        arr[i] = []
+    for (let i = 0; i < param1; i++) {
         for (let j = 0; j < param1; j++) {
-            arr[i][j] = num
+            arr.push([i][j] = num)
             num++
-
         }
-        console.log(arr[i])
     }
+    console.log(arr)
 }
 
 spiral(5)
