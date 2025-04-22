89f45a1c69f2ee9cc06f2a200ca66f760de89264
MuhamadFathir <fathirrizqii866@gmail.com>
2025-04-22 17:37:44 +0700

    new

--- c9.js
+++ c9.js
@@ -1,14 +1,52 @@
-function spiral(param1) {
-    let arr = []
+function spiral(size) {
+    let matrix = []
     let num = 0
-    for (let i = 0; i < param1; i++) {
-        arr[i] = []
-        for (let j = 0; j < param1; j++) {
-            arr[i][j] = num
+    for (let i = 0; i < size; i++) {
+        matrix[i] = []
+        for (let j = 0; j < size; j++) {
+            matrix[i][j] = num
             num++
         }
     }
-    console.log(arr)
+
+    let atas = size
+    let bawah = 0
+    let result = []
+    let x = 0, y = 0
+    while (result.length < size * size) {
+
+
+        // result.push(matrix)
+        for (; x < atas; x++) {
+            result.push(matrix[y][x])
+        }
+
+        x--
+        y++
+        for (; y < atas; y++) {
+            result.push(matrix[y][x])
+        }
+        y--
+        x--
+
+        for (; x >= bawah; x--) {
+            result.push(matrix[y][x])
+        }
+        
+        x++
+        y--
+
+        for (; y > bawah; y--) {
+            result.push(matrix[y][x])
+        }
+        x++
+        y++
+        atas--
+        bawah++
+    }
+    console.log(result)
 }
 
 spiral(5)
+spiral(6)
+spiral(7)
