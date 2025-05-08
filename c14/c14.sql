6c982c3a33fa2adfc7f3502293ea9ce9483f0e0a
MuhamadFathir <fathirrizqii866@gmail.com>
2025-05-08 15:02:02 +0700

    done

--- c14/c14.sql
+++ c14/c14.sql
@@ -19,12 +19,15 @@ insert into mahasiswa (nim, nama_mahasiswa, alamat, kode_jurusan) values
 ('5237', 'aria', 'sumedang', '62201');
 
 CREATE TABLE mata_kuliah (
-    kode_matkul character(6) primary key not null, 
-    nama_matkul varchar(100) not null, SKS varchar(20) not null);
+    id_matkul character(3) primary key not null, 
+    nama_matkul varchar(100) not null, SKS varchar(20) not null, 
+    nim character(4) not null, foreign key(nim) references mahasiswa(nim));
 
-insert into mata_kuliah (kode_matkul, nama_matkul, sks) values 
-('MT201', 'Matematika', '2'),
-('BI101', 'bahasa indonesia', '2');
+insert into mata_kuliah (id_matkul, nama_matkul, sks, nim) values 
+('001', 'Matematika', '12', '7389'),
+('002', 'bahasa indonesia', '10', '2536'),
+('003', 'bahasa indonesia', '12', '5237');
+
 
 CREATE TABLE dosen (
     id_dosen character(4) primary key not null, 
@@ -37,28 +40,32 @@ insert into dosen (id_dosen, nama_dosen) values
 create table belajar (
     id character(3) primary key not null, 
     nim character(4) not null, 
+    id_dosen character(4) not null, 
     kode_matkul character(6) not null, 
     nilai character(3) not null, 
+    foreign key(id_dosen) references dosen(id_dosen),
     foreign key(nim) references mahasiswa(nim), 
     foreign key(kode_matkul) references mata_kuliah(kode_matkul)
 );
 
 insert into belajar values 
-('001', '7389', 'MT201', '85'),
-('002', '2536', 'MT201', '89'),
-('003', '5237', 'BI101', '90');
+('001', '7389', '0001', '001', 'A'),
+('002', '2536', '0001', '002', 'C'),
+('003', '5237', '0002', '003', 'D');
 
-create table mengajar (
-    id character(2) primary key not null, 
-    id_dosen character(4) not null, 
-    kode_matkul character(6) not null, 
-    foreign key(id_dosen) references dosen(id_dosen),
-    foreign key(kode_matkul) references mata_kuliah(kode_matkul)
-);
 
-insert into mengajar values 
-('01', '001', 'BI101'),
-('02', '002', 'MT201');
+
+-- koreksi 
+alter table mahasiswa add column umur text;
+update mahasiswa set umur = '18' where nim = '7389';
+update mahasiswa set umur = '20' where nim = '2536';
+update mahasiswa set umur = '19' where nim = '5237';
+
+
+update mata_kuliah set sks = '12' where id_matkul = '003';
+update mata_kuliah set sks = '10' where id_matkul = '002';
+update mata_kuliah set nama_matkul = 'data mining' where id_matkul = '002';
+
 
 
 
